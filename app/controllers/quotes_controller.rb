d48857b3ea1368d661483b0383d35551f6a24927
Aaron Washburn <aaron@it-kutch.com>
2015-01-22 00:36:22 +0000

    Add modal for contribute

--- app/controllers/quotes_controller.rb
+++ app/controllers/quotes_controller.rb
@@ -3,9 +3,6 @@ class QuotesController < ApplicationController
 	  @quote = Quote.order("Random()").first
 	end
 
-	def new
-	  @quote = Quote.new
-	end
 
 	def create
 	  @quote = Quote.create(quote_params)
