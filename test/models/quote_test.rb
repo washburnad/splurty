7aa4d9a5c7e7980bfe463ed5f812b02f6b89c3e4
Aaron Washburn <aaron@it-kutch.com>
2015-02-02 19:01:19 +0000

    Add quote.unique_tag and pass test

--- test/models/quote_test.rb
+++ test/models/quote_test.rb
@@ -1,7 +1,10 @@
 require 'test_helper'
 
 class QuoteTest < ActiveSupport::TestCase
-  test "the truth" do 
-  	assert true
+  test "unique tag" do 
+  	quote = Quote.create(:author => "Aaron Washburn", :saying => "Just do it.")
+  	expected = "AW#" + quote.id.to_s
+  	actual = quote.unique_tag
+  	assert_equal expected, actual
   end
 end
